9df2cf6c75625a34addf5183b8e59f8b65307f0d
Sindre <sindre.glomnes@gmail.com>
2022-08-22 13:23:01 +0200

    Exercise1

---
+++ Exercise1/app/src/main/java/com/example/exercise1/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.exercise1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(meny: Menu): Boolean{
+        super.onCreateOptionsMenu(meny)
+        meny.add("Sindre")
+        meny.add("Glomnes")
+        Log.d("INFT2501","meny laget") //vises i LogCat
+        return true // gjor at menyen vil vises
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean{
+        if (item.title.equals("Sindre")) {
+            Log.d("Leksjon", "Valg 'Sindre' er trykket av brukeren")
+        }
+        if (item.title.equals("Glomnes")) {
+            Log.d("Leksjon", "Valg 'Glomnes' er trykket av brukeren")
+        }
+        return true
+    }
+}
