8c0d5b234b261e3148b3371e31c39a030a68a23a
Sindre <sindre.glomnes@gmail.com>
2022-08-24 11:45:37 +0200

    Toast

--- exercise2/app/src/main/java/com/example/exercise2/MainActivity.kt
+++ exercise2/app/src/main/java/com/example/exercise2/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.exercise2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClickVisAlertDialog(v: View?) {
+        val value = (0..100).random().toString();
+        Toast.makeText(applicationContext, value, Toast.LENGTH_SHORT).show()
     }
 }
