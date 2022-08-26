56ec7592319c24c26b71bac7faa3c28957d035c7
Sindre <sindre.glomnes@gmail.com>
2022-08-26 14:32:47 +0200

    layout fix

---
+++ Leksjon 2/app/src/main/java/inft2501/exercise2/MainActivity2.kt
@@ -0,0 +1,11 @@
+package inft2501.exercise2
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity2  : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main2_activity)
+    }
+}
