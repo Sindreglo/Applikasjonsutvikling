1b028b5f6eb1cc1e3cb0f4353c839bf306d11315
Sindre <sindre.glomnes@gmail.com>
2022-09-14 17:26:42 +0200

    image size gucci

--- Exercise4/app/src/main/java/com/sindrgl/exercise4/MainActivity.kt
+++ Exercise4/app/src/main/java/com/sindrgl/exercise4/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.sindrgl.exercise4
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -12,10 +13,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import com.sindrgl.exercise4.ui.theme.Exercise4Theme
 import coil.compose.ImagePainter
+import coil.compose.rememberAsyncImagePainter
 import coil.compose.rememberImagePainter
 
 class ImagesViewModel : ViewModel() {
@@ -30,9 +34,33 @@ class ImagesViewModel : ViewModel() {
             "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1474169725i/15881._SY180_.jpg"
         ),
         ImageObj(
-            "Nr 3",
-            "https://m.psecn.photoshelter.com/img-get2/I0000H0rIQzSXuNA/sec=/fit=1200x1200/I0000H0rIQzSXuNA.jpg"
+            "Harry Potter and the Prisoner of Azkaban",
+            "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1630547330i/5._SY180_.jpg"
         ),
+
+
+        ImageObj(
+            "Harry Potter and the Goblet of Fire",
+            "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1554006152i/6._SX120_.jpg"
+        ),
+        ImageObj(
+            "Harry Potter and the Order of the Phoenix",
+            "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1660911061i/2._SY180_.jpg"
+        ),
+        ImageObj(
+            "Harry Potter and the Half-Blood Prince",
+            "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1587697303i/1._SX120_.jpg"
+        ),
+
+        ImageObj(
+            "Harry Potter and the Deathly Hallows",
+            "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1661250233i/136251._SY180_.jpg"
+        ),
+        ImageObj(
+            "Harry Potter and the Half-Blood Prince",
+            "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1587697303i/1._SX120_.jpg"
+        ),
+
     )
         private set
 
@@ -78,52 +106,55 @@ fun Main(
                     .weight(1.0f)
                     .fillMaxWidth()
             ) {
-                Greeting("sindre")
+                imageFromURL()
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Composable
 fun List(images: List<ImageObj>) {
     LazyColumn {
         items(images.size) { index ->
-            ShowImage(images[index])
-            Spacer(modifier = Modifier.height(5.dp))
+            Button(onClick = { Log.w("click", "Clicked") }, modifier = Modifier.fillMaxWidth()) {
+                Text(text = images[index].label)
+            }
         }
     }
 }
 
 @Composable
-fun ShowImage(image: ImageObj) {
-    val painter = rememberImagePainter(
-        data = image.url,
-        builder = {
-            crossfade(true)
-        }
-    )
+fun imageFromURL() {
+    // on below line we are creating a column,
+    Column(
+        // in this column we are adding modifier
+        // to fill max size, mz height and max width
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxHeight()
+            .fillMaxWidth()
+            // on below line we are adding
+            // padding from all sides.
+            .padding(10.dp),
+        // on below line we are adding vertical
+        // and horizontal arrangement.
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // on below line we are adding image for our image view.
+        Image(
+            // on below line we are adding the image url
+            // from which we will  be loading our image.
+            painter = rememberAsyncImagePainter("https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1587697303i/1._SX120_.jpg"),
 
-    Box {
-        Column {
-            Image(
-                painter = painter,
-                contentDescription = image.label,
-                modifier = Modifier.fillMaxWidth()
-            )
-            Text(image.label)
-        }
+            // on below line we are adding content
+            // description for our image.
+            contentDescription = "gfg image",
 
-        when (painter.state) {
-            is ImagePainter.State.Loading -> {
-                CircularProgressIndicator(Modifier.align(Alignment.Center))
-            }
-            else -> {
-            }
-        }
+            // on below line we are adding modifier for our
+            // image as wrap content for height and width.
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxHeight()
+        )
     }
 }
