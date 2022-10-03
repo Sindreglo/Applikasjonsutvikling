e18b833cf013233433e8966807c5d19683ef9c22
Sindre <sindre.glomnes@gmail.com>
2022-10-03 17:43:14 +0200

    testing

--- Exercise5/app/src/main/java/com/sindrgl/exercise5/MainActivity.kt
+++ Exercise5/app/src/main/java/com/sindrgl/exercise5/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.sindrgl.exercise5
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.sp
 import com.sindrgl.exercise5.ui.theme.Exercise5Theme
@@ -23,9 +25,15 @@ import kotlinx.coroutines.launch
 
 const val URL = "https://bigdata.idi.ntnu.no/mobil/tallspill.jsp"
 
+enum class AppView {
+    START,
+    GAME,
+}
+
 class MainActivity : ComponentActivity() {
 
     private val network: HttpWrapper = HttpWrapper(URL)
+    private var gamestate = AppView.START
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +44,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    UserInfo()
+                    Scaffold {
+                        Column {
+                            if (gamestate == AppView.START) {
+                                UserInfo()
+                            } else {
+                                GameOn()
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -67,6 +83,7 @@ class MainActivity : ComponentActivity() {
     private fun setResult(response: String?) {
         if (response != null) {
             Log.w("Response", response)
+            gamestate = AppView.GAME
         }
     }
 
@@ -78,10 +95,7 @@ class MainActivity : ComponentActivity() {
         fun add() {
             val requestParameters = mapOf<String, String>("navn" to nameState.value.text,
                 "kortnummer" to cardState.value.text,)
-
             performRequest(HTTP.GET, requestParameters)
-            Log.w("Friend", nameState.value.text)
-            Log.w("Friend", cardState.value.text)
         }
         Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()) {
             Text(text = "Free_Bitcoins.exe", fontSize = 25.sp)
@@ -95,6 +109,21 @@ class MainActivity : ComponentActivity() {
                 label = { Text("Kortnummer") }
             )
             Button(onClick = { add() }) {
+                Text("Start!")
+            }
+        }
+    }
+
+    @Composable
+    fun GameOn() {
+        val nameState = remember { mutableStateOf(TextFieldValue()) }
+        Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()) {
+            Text(text = "Free_Bitcoins.exe", fontSize = 25.sp)
+            TextField(
+                value = nameState.value,
+                onValueChange = { nameState.value = it },
+                label = { Text("Navn") })
+            Button(onClick = { }) {
                 Text("Start!")
             }
         }
